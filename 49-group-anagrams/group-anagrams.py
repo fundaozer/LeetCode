26c6a60bc36bbebebabe351ca12cbd003386fb5c
Funda Özer <ozerfunda45@gmail.com>
2026-06-18 17:12:19 +0300

    Time: 29 ms (25.39%) | Memory: 16.2 MB (52.75%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        groups={}
+        for str in strs:
+            key= "".join(sorted(str))
+            if key not in groups:
+                groups[key]=[]
+            groups[key].append(str)
+        return list(groups.values())
+        
+        
