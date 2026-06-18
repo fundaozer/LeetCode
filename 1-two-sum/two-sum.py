b432f750e2c365a513678b6c9afb99f777287a48
Funda Özer <ozerfunda45@gmail.com>
2026-06-18 16:28:26 +0300

    Time: 0 ms (100.00%) | Memory: 13 MB (81.92%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,8 +1,10 @@
 class Solution(object):
     def twoSum(self,nums, target):
-        seen={}
+        hash_map={}
+
         for i,num in enumerate(nums):
             needed=target-num
-            if needed in seen:
-                return [seen[needed],i]
-            seen[num]=i
+            if needed in hash_map:
+                return(hash_map[needed],i)
+            hash_map[num]=i
+
